364ec35e94065aefb016528e9f378c84f8f0cf6e
Megha Mahna <meghamahna04@gmail.com>
2020-04-17 15:32:29 -0400

    Translator

--- app/src/main/java/com/example/capstone_elibrary/TranslatorFragment.java
+++ app/src/main/java/com/example/capstone_elibrary/TranslatorFragment.java
@@ -1,15 +1,50 @@
 package com.example.capstone_elibrary;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static com.example.capstone_elibrary.TranslatorGlobalVars.LANGUAGE_CODES;
+import static com.example.capstone_elibrary.TranslatorGlobalVars.BASE_REQ_URL;
+import static com.example.capstone_elibrary.TranslatorGlobalVars.DEFAULT_LANG_POS;
+
 public class TranslatorFragment extends Fragment {
+
+    public static final String LOG_TAG = MainActivity.class.getName();
+    private static final int REQ_CODE_SPEECH_INPUT = 1;
+    private Spinner mSpinnerLanguageFrom;                   //    Dropdown list for selecting base language (From)
+    private Spinner mSpinnerLanguageTo;                     //    Dropdown list for selecting translation language (To)
+    private String mLanguageCodeFrom = "en";                //    Language Code (From)
+    private String mLanguageCodeTo = "en";                  //    Language Code (To)
+    private EditText mTextInput;                            //    Input text ( in From language )
+    private TextView mTextTranslated;                       //    Output Translated text ( in To language )
+    private Dialog process_tts;                             //    Dialog box for Text to Speech Engine Language Switch
+    HashMap<String, String> map = new HashMap<>();
+    volatile boolean activityRunning;
 
     @Nullable
     @Override
@@ -17,5 +52,151 @@ public class TranslatorFragment extends Fragment {
         return inflater.inflate(R.layout.translator_frag, container, false);
 
 
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        activityRunning = true;
+        TextView mEmptyTextView = (TextView) getView().findViewById(R.id.empty_view_not_connected);
+        mSpinnerLanguageFrom = (Spinner) getView().findViewById(R.id.spinner_language_from);
+        mSpinnerLanguageTo = (Spinner) getView().findViewById(R.id.spinner_language_to);
+        Button mButtonTranslate = (Button) getView().findViewById(R.id.button_translate);         //      Translate button to translate text
+        ImageView mImageSwap = (ImageView) getView().findViewById(R.id.image_swap);               //      Swap Language button to swap languages
+        ImageView mClearText = (ImageView) getView().findViewById(R.id.clear_text);               //      Clear button to clear text fields
+        mTextInput = (EditText) getView().findViewById(R.id.text_input);
+        mTextTranslated = (TextView) getView().findViewById(R.id.text_translated);
+        mTextTranslated.setMovementMethod(new ScrollingMovementMethod());
+//        process_tts = new Dialog(MainActivity.this);
+//        process_tts.setContentView(R.layout.dialog_processing);
+//        process_tts.setTitle("Switching Language");
+//        TextView title = (TextView) process_tts.findViewById(android.R.id.title);
+
+        //  CHECK INTERNET CONNECTION
+        if (!isOnline()) {
+            mEmptyTextView.setVisibility(View.VISIBLE);
+        } else {
+            mEmptyTextView.setVisibility(View.GONE);
+            //  GET LANGUAGES LIST
+            new GetLanguages().execute();
+
+            //  TRANSLATE
+            mButtonTranslate.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String input = mTextInput.getText().toString();
+                    new TranslateText().execute(input);
+                }
+            });
+            //  SWAP BUTTON
+            mImageSwap.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String temp = mLanguageCodeFrom;
+                    mLanguageCodeFrom = mLanguageCodeTo;
+                    mLanguageCodeTo = temp;
+                    int posFrom = mSpinnerLanguageFrom.getSelectedItemPosition();
+                    int posTo = mSpinnerLanguageTo.getSelectedItemPosition();
+                    mSpinnerLanguageFrom.setSelection(posTo);
+                    mSpinnerLanguageTo.setSelection(posFrom);
+                    String textFrom = mTextInput.getText().toString();
+                    String textTo = mTextTranslated.getText().toString();
+                    mTextInput.setText(textTo);
+                    mTextTranslated.setText(textFrom);
+                }
+            });
+            //  CLEAR TEXT
+            mClearText.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mTextInput.setText("");
+                    mTextTranslated.setText("");
+                }
+            });
+            //  SPINNER LANGUAGE FROM
+            mSpinnerLanguageFrom.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    mLanguageCodeFrom = LANGUAGE_CODES.get(position);
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                    Toast.makeText(getActivity().getApplicationContext(), "No option selected", Toast.LENGTH_SHORT).show();
+                }
+            });
+            //  SPINNER LANGUAGE TO
+            mSpinnerLanguageTo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    mLanguageCodeTo = LANGUAGE_CODES.get(position);
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                    Toast.makeText(getActivity().getApplicationContext(), "No option selected", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    //  CHECK INTERNET CONNECTION
+    public boolean isOnline() {
+        try {
+            ConnectivityManager connectivityManager = (ConnectivityManager) getActivity().getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            return networkInfo != null && networkInfo.isAvailable() && networkInfo.isConnected();
+        } catch (Exception e) {
+            Log.e(LOG_TAG, e.getMessage());
+        }
+        return false;
+    }
+
+    //  SUBCLASS TO TRANSLATE TEXT ON BACKGROUND THREAD
+    private class TranslateText extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... input) {
+            Uri baseUri = Uri.parse(BASE_REQ_URL);
+            Uri.Builder uriBuilder = baseUri.buildUpon();
+            uriBuilder.appendPath("translate")
+                    .appendQueryParameter("key", getString(R.string.API_KEY))
+                    .appendQueryParameter("lang", mLanguageCodeFrom + "-" + mLanguageCodeTo)
+                    .appendQueryParameter("text", input[0]);
+            Log.e("String Url ---->", uriBuilder.toString());
+            return TranslatorQueryUtils.fetchTranslation(uriBuilder.toString());
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (activityRunning) {
+                mTextTranslated.setText(result);
+            }
+        }
+    }
+
+    //  SUBCLASS TO GET LIST OF LANGUAGES ON BACKGROUND THREAD
+    private class GetLanguages extends AsyncTask<Void, Void, ArrayList<String>> {
+        @Override
+        protected ArrayList<String> doInBackground(Void... params) {
+            Uri baseUri = Uri.parse(BASE_REQ_URL);
+            Uri.Builder uriBuilder = baseUri.buildUpon();
+            uriBuilder.appendPath("getLangs")
+                    .appendQueryParameter("key", getString(R.string.API_KEY))
+                    .appendQueryParameter("ui", "en");
+            Log.e("String Url ---->", uriBuilder.toString());
+            return TranslatorQueryUtils.fetchLanguages(uriBuilder.toString());
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<String> result) {
+            if (activityRunning) {
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, result);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                mSpinnerLanguageFrom.setAdapter(adapter);
+                mSpinnerLanguageTo.setAdapter(adapter);
+                //  SET DEFAULT LANGUAGE SELECTIONS
+                mSpinnerLanguageFrom.setSelection(DEFAULT_LANG_POS);
+                mSpinnerLanguageTo.setSelection(DEFAULT_LANG_POS);
+            }
+        }
     }
 }
